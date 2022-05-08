b394ec21379ec0434a6a1dc2ffe98beac199c03f
AlekseyLesnichiy <91471392+AlekseyLesnichiy@users.noreply.github.com>
2022-05-08 19:34:26 +0300

    сделал стили

--- src/js/card.js
+++ src/js/card.js
@@ -1,12 +1,17 @@
 //  backdrop_path  original_title release_date   genres (array[objects])
 import FilmsApiService from './fetch-raduka';
+import GetGenres from './getGenres';
 
 const refs = {
   buildFilmGallery: document.querySelector('.buildFilmGallery'),
   filmsGalleyDiv: document.querySelector('.main-gallery-lisnichyi'),
+  // input:document.querySelector("#search-box")
 };
 
 const filmsApiService = new FilmsApiService();
+const getGenres = new GetGenres();
+
+getGenres.Genres().then(res => console.log('results', res));
 
 refs.buildFilmGallery.addEventListener('click', onClick);
 
@@ -23,17 +28,20 @@ function onClick(event) {
 
 function makeFilmCard(films) {
   const markup = films.data.results.map(
-    ({ poster_path, original_title, release_date, genre_ids }) => {
+    ({ poster_path, original_title, release_date, genre_ids, id, vote_average }) => {
         if (genre_ids.length <=2) {
                  return `
         <li class="card__container">
     <img
       class="card__picture"
+      filmId ="${id}"
       src="https://image.tmdb.org/t/p/w500/${poster_path}"
       alt="${original_title}"
     />
       <p class="card__title"><span class="card__title--name"> ${original_title}</span>
-     <span class="card__title--genres">${genre_ids[0]},${genre_ids[1]}| ${release_date}</span></p>
+     <span class="card__title--genres">${genre_ids[0]},${genre_ids[1]}| ${release_date}  <span class="card__filmRaiting"> ${vote_average }</span></span>
+      
+     </p>
         </li>`
         }
         else {
@@ -41,12 +49,14 @@ function makeFilmCard(films) {
         <li class="card__container">
     <img
       class="card__picture"
+      filmId ="${id}"
       src="https://image.tmdb.org/t/p/w500/${poster_path}"
       alt="${original_title}"
     /> 
     <p class="card__title"><span class="card__title--name"> ${original_title}</span>
-     <span class="card__title--genres">${genre_ids[0]},${genre_ids[1]}, Other | ${release_date}</span></p>
-        
+     <span class="card__title--genres">${genre_ids[0]},${genre_ids[1]}, Other | ${release_date}  <span class="card__filmRaiting"> ${vote_average }</span></span>
+    
+     </p>
         </li>`
         }
       }).join(" ");
