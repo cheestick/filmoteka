de7efe66191e8d8fdb246a6ea7686ac736479179
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-05-12 11:05:47 +0300

    spinner.html changed. heaer controller dev started

---
+++ src/js/Header/HeaderController.js
@@ -0,0 +1,14 @@
+const REF = {
+  LOGO: document.querySelector('#site-logo'),
+  HOME: document.querySelector('#home'),
+  LIBRARY: document.querySelector('#library'),
+  PAGE: document.querySelector('.page-container'),
+  WATCHED: document.querySelector('.tab-watched'),
+  QUEUE: document.querySelector('.tab-queue'),
+};
+
+class HeaderController {
+  constructor() {}
+}
+
+export default HeaderController;
