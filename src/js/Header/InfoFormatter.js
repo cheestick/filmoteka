31f2d2daeb9f36f0db462f18129f71d3c959d39b
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-05-13 23:09:55 +0300

    genres formatter special case refactored

--- src/js/Header/InfoFormatter.js
+++ src/js/Header/InfoFormatter.js
@@ -1,5 +1,5 @@
 export function formatGenresData(genres) {
-  const genreNames = genres.map(genre => genre.name);
+  const genreNames = genres.map(genre => (genre?.name ? genre.name : genre)); //special case, hmm... not good at all
   return genreNames.length <= 2
     ? genreNames.join(', ')
     : genreNames.slice(0, 2).join(', ').concat(', other');
