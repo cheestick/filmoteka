0ddf5768c36f7c3d22c68fed8483ca1f9f24e1e6
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2022-05-15 08:32:09 +0300

    _v10_genres_convertion

--- src/js/Header/InfoFormatter.js
+++ src/js/Header/InfoFormatter.js
@@ -12,3 +12,20 @@ export function formatReleaseYearData(releaseYear) {
 export function formatNumericalToFixed(number) {
   return number?.toFixed(1);
 }
+
+export function formatConvertedGenres(genres) {
+  const genresNamesArray = JSON.parse(localStorage.getItem('GenresArray'));
+
+  const translatedGenres = genres.map(item => {
+    const searchValue = item;
+    const nameObject = genresNamesArray.find(option => option.id === searchValue);
+    return (item = nameObject.name);
+  });
+
+  // console.log(translatedGenres);
+
+  const genreNames = translatedGenres.map(genre => (genre?.name ? genre.name : genre)); //special case, hmm... not good at all
+  return genreNames.length <= 2
+    ? genreNames.join(', ')
+    : genreNames?.slice(0, 2).join(', ').concat(', other');
+}
