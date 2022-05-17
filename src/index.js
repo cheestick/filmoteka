f40cf43e12ccfbec9828679c96ed4937266fada5
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-05-17 12:48:24 +0300

    no search results notification added

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import './sass/main.scss';
 import './js/Header/HeaderController';
 import './js/pagination';
 import './js/modal';
+import { Notify } from 'notiflix';
+
+Notify.init({ position: 'center-top', timeout: 1300 });
